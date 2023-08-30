b1a59865fe02f67f18ddf06bd274e22c7fa41e0c
cbarbes <cbarbes02@gmail.com>
2023-08-30 18:56:50 -0400

    Added some comments to the lab code.

--- Lab01/Program01/main.cpp
+++ Lab01/Program01/main.cpp
@@ -18,23 +18,27 @@ void swap(t *, int, int);
 template<class t>
 void Sort(t, int);
 
-// templated 
+// templated function to check if an array is sorted
 template<class t>
 bool Sorted(t *, int n);
 
 int main(){
 
-	int ArrSize = 0;
+	int ArrSize = 0; // init the array size
 
+	// take input from the user
 	cout<<"Please input the size of the array "<<endl;
 	cin>>ArrSize;
 
+	// initialize the array dynamically
 	int *randArray = new int[ArrSize];
 
+	// loop through array and initialize all values to random number between -1000 and 1000
 	for(int i = 0; i<ArrSize; i++){
 		randArray[i] = (rand()%2001)-1000;
 	}
-         
+        
+	// time a the sorting process
 	auto start = high_resolution_clock::now();
 	// Process to be timed.
 	Sort(randArray, ArrSize);
@@ -49,18 +53,16 @@ int main(){
 	{
 		cout<< "Sorted"<<endl;
 	}
-	/*
-	high_resolution_clock::time_point start = high_resolution_clock::now();
-	// Process to be timed.
-	high_resolution_clock::time_point stop = high_resolution_clock::now();
-	duration<double> duration = duration_cast<microseconds>(stop - start);
-	cout << "Time to sort: " << duration.count() / 1000000.0 << " seconds" << endl;
-	*/
+	
 	delete [] randArray;
 
 	return 0;
 }
 
+/***************************************************************
+*  Description: This function swaps 2 values in an array
+*  Parameters: The array, The first index, The second index
+***************************************************************/
 template<class t>
 void swap(t *arr, int n1, int n2){
 	t temp = arr[n1];
@@ -68,25 +70,33 @@ void swap(t *arr, int n1, int n2){
 	arr[n2] = temp;
 }
 
+/**********************************************************************
+ *  Description: This function sorts a given array in ascending order
+ *  Parameters: the array and the size
+ *********************************************************************/
 template<class t>
 void Sort(t arr, int n){
 	int i, minIdx = 0;
 	
 	for(i = 0; i<n; i++){
 
-		minIdx = i;
+		minIdx = i; // set the target 
 		for(int j = i; j<n; j++){
 			if(arr[j] < arr[minIdx]){
-				minIdx = j;
+				minIdx = j;// find the swap
 			}
 		}
-		if(minIdx != i){
+		if(minIdx != i){ // complete the swap as long as the number is different
 			swap(arr, i, minIdx);
 		}
 
 	}
 }
 
+/**********************************************************************
+ * Description: This function Checks whether an array is sorted
+ * Parameters: the array and the size
+ **********************************************************************/
 template<class t>
 bool Sorted(t *arr, int n){
 
