47f34cd8be939116f7f1348674b90553f6b4d90b
cbarbes <cbarbes02@gmail.com>
2023-09-07 21:36:26 -0400

    Added the rest of the code.

--- Homework1/Program6/Thing1.h
+++ Homework1/Program6/Thing1.h
@@ -1,3 +1,10 @@
+/***************************************************************************************
+* Author: Cole Barbes
+* Creation Date: 09/03/23
+* Last Update: 09/07/23
+* Description: This file contains 2 classes to implement a Linked List.
+* Notes: Linked Lists are interesting logically because it makes sense to the brain
+ ***************************************************************************************/
 #ifndef THING1_H
 #define THING1_H
 
@@ -5,82 +12,113 @@
 
 using namespace std;
 
+// List Node structure to create nodes
 template<class T>
 class ListNode{
 	public:
-		T value;
-		ListNode<T>* next;
-		ListNode<T>* previous;
+		T value; // templated value for the node
+		ListNode<T>* next; // next pointer
+		ListNode<T>* previous; // previous pointer
 		ListNode(){
 			next = nullptr;
 			previous = nullptr;
 		}
 };
 
-
+// class structure to implement the linked list
 template<class T>
 class Thing1{
 	protected:
-		ListNode<T>* head;
-		ListNode<T>* tail;
+		ListNode<T>* head; // head pointer
+		ListNode<T>* tail; // tail pointer
 	public:
-		Thing1();
-		~Thing1();
-		void pushback(T);
+		Thing1(); // constructor
+		~Thing1(); // destructor
+		// push value to the end of list
+		void pushback(T); 
+		// pop value from the back
 		T popback();
+		// push value to the front of the list
 		void pushfront(T);
+		// pop value from the front
 		T popfront();
+		// return the size of the list
 		int size();
+		// clear the nodes from the list
 		void clear();
+		// print forward and backward
 		void printforward(bool print = false);
 		void printbackward(bool print = false);
-		bool find(T);
+		
+		// find the value
+		bool find(T);  
 		int findpos(T);
-		bool isempty();
+
+		bool isempty(); // whether list is empty
+
+		// setter and getter
 		T getValue(int);
 		void setValue(int, T);
+
+		// friend output stream overload
 		template<class C>
 		friend ostream& operator<<(ostream&, Thing1<C>&);
 };
 
+
+/********************************************************************
+ * Constructor
+ ********************************************************************/
 template<class T>
 Thing1<T>::Thing1()
 {
-head = nullptr;
-tail = nullptr;
+	head = nullptr;
+	tail = nullptr;
 }
 
+/**
+ * Deconstructor
+ **/
 template<class T>
 Thing1<T>::~Thing1()
 {
-clear();
+	clear();
 }
 
+/**
+ * Description: Insert value at the end of the list
+ * Parameters: Templated value for the node
+ */
 template<class T>
 void Thing1<T>::pushback(T var)
 {
+	// create the node
 	ListNode<T>* tempNode = new ListNode<T>();
 	tempNode->value = var;
 
-	if(!head){
+	if(!head){ // if empty fill it 
 		head = tempNode;
 		tail = tempNode;
 	}
-	else{	
+	else{ // otherwise add the new node to the end	
 		tail->next = tempNode;
 		tempNode->previous = tail;
 		tail = tempNode;
+		tail->next = nullptr;
 	}
 }
-
+/*
+ * Description: Delete the last node
+ * Return: Return the Value being deleted
+ */
 template<class T>
 T Thing1<T>::popback()
 {
-	if(!head){
+	if(!head){ // if empty send back default value
 		T temp = T();
 		return temp;
 	}
-	else{
+	else{ // else Remove the node and return it
 		T temp = tail->value;
 		ListNode<T>* tempNode = tail;
 		tail = tail->previous;
@@ -89,14 +127,18 @@ T Thing1<T>::popback()
 		return temp;
 	}
 }
-
+/*
+ * Description: Insert value in the front of the list
+ * Parameters: The value being inserted
+ */
 template<class T>
 void Thing1<T>::pushfront(T var)
 {
-	if(!head){
+	if(!head){ // if empty place value at the head
 		head->value = var;
+		
 	}
-	else{
+	else{ // push the value to the front shifting the head to it
 		ListNode<T>* tempNode = new ListNode<T>();
 		tempNode->value = var;
 		tempNode->next = head;
@@ -105,14 +147,17 @@ void Thing1<T>::pushfront(T var)
 	}
 }
 
+/* Description: Delete the value in the front of the list and return it
+ * Return: The templated value 
+ */
 template<class T>
 T Thing1<T>::popfront()
 {
-	if(!head){
+	if(!head){ // if empry return default value
 		T temp = T();
 		return temp;
 	}
-	else{
+	else{ // remove node and return the value
 		ListNode<T>* tempNode = head;
 		T temp = head->value;
 		head = head->next;
@@ -124,14 +169,18 @@ T Thing1<T>::popfront()
 	}
 }
 
+/* Description: Get the size of the list
+ * Return: the integer size 
+ */
 template<class T>
 int Thing1<T>::size()
 {
 	int count = 0;
 
+	// create temp node pointer to start traversal
 	ListNode<T>* nodePtr = head;
 
-	while(nodePtr){
+	while(nodePtr){ // while nodes left, count nodes
 		count++;
 		nodePtr = nodePtr->next;
 	}
@@ -139,21 +188,30 @@ int Thing1<T>::size()
 	return count;
 }
 
+/* Description: Clear the list of all nodes and delete the memory
+ * Notes: This can be used to descruct the list
+ */
 template<class T>
 void Thing1<T>::clear()
 {
+	// set up the traversal pointer
 	ListNode<T>* nodePtr = head;
 	ListNode<T>* tempPtr;
-	while(nodePtr){
+
+	while(nodePtr){ // if more nodes traverse farther
 		tempPtr = nodePtr->next;
 		delete nodePtr;
 		nodePtr = tempPtr;
 	}
+
 	head = nullptr;
 	tail = nullptr;
 
 }
 
+/* Description: Print the list in forward order
+ * Parameters: Boolean Value true print line by line false print on one line
+ */
 template<class T>
 void Thing1<T>::printforward(bool print)
 {
@@ -173,16 +231,20 @@ void Thing1<T>::printforward(bool print)
 	
 }
 
+/* Description: Print the list in backward order
+ * Parameters: Boolean value true print on seperate lines and false print on one line
+ */
 template<class T>
 void Thing1<T>::printbackward(bool print)
 {
+	// set the pointer to the back for traversing
 	ListNode<T>* nodePtr = tail;
 
-	while(nodePtr){
-		if(print){
+	while(nodePtr){ // while nodes print
+		if(print){ // seperate lines
 			cout<<nodePtr->value<<endl;
 		}
-		else{
+		else{ // individual lines
 			cout<<nodePtr->value<<" ";
 		}
 		nodePtr = nodePtr->previous;
@@ -192,30 +254,43 @@ void Thing1<T>::printbackward(bool print)
 
 }
 
+/* Description: Find the node with the given value if in the list return true
+ * Parameters: Templated value 
+ * return: boolean value
+ */
 template<class T>
 bool Thing1<T>::find(T val)
 {
+	// pointer to traverse with starting at the head
 	ListNode<T>* nodePtr = head;
 
-	while(nodePtr->value != val && nodePtr != tail){
+	while(nodePtr->value != val && nodePtr != tail){ // if value isn't found or tail isn't reached 
 		nodePtr = nodePtr->next;
 	}
+
 	return nodePtr->value == val;
-	
+
 }
 
+
+/* Description: Find the node with the given value and return the indices
+ * Parameters: Templated value
+ * Return: the integer position
+ */
 template<class T>
 int Thing1<T>::findpos(T val)
 {
+	// create the pointer for traversal
 	ListNode<T>* nodePtr = head;
 
-	int count = 0;
+	int count = 0; // create the count 
 
-	while(nodePtr->value != val && nodePtr != tail){
+	while(nodePtr->value != val && nodePtr != tail){ // while the value isn't reached or the tail isnt reached
 		nodePtr = nodePtr->next;
-		count++;
+		count++; // count the number of nodes
 	}
-	if(nodePtr->value==val){
+
+	if(nodePtr->value==val){ // if its the value then return the count
 		return count;
 	}
 	else {
@@ -224,25 +299,34 @@ int Thing1<T>::findpos(T val)
 
 }
 
+/* Description: find out if the list is empty
+ * Return: boolean whether the list is empty true 
+ */
 template<class T>
 bool Thing1<T>::isempty()
 {
 	return (!head);
 }
 
+
+/* Description: Get the value in the indicated position
+ * Parameters: The indices
+ * Return: The value
+ */
 template<class T>
 T Thing1<T>::getValue(int pos)
 {
 	int count = 0;
 
+	// create the node to use when traversing
 	ListNode<T>* nodePtr = head;
 
-	while(nodePtr && count != pos){
+	while(nodePtr && count != pos){ // while the node is valid and the count hasnt reached the position
 		nodePtr = nodePtr->next;
 		count++;
 	}
 
-
+	// if count reached the position return the value
 	if(count == pos){
 		return nodePtr->value;
 	}
@@ -253,30 +337,41 @@ T Thing1<T>::getValue(int pos)
 	}
 }
 
+
+/* Description: set the value of a node at a given position
+ * Parameters: The position and the Templated value
+ */
 template<class T>
 void Thing1<T>::setValue(int pos, T var)
 {
 	int count = 0;
 
+	// create the node to traverse the list with 
 	ListNode<T>* nodePtr = head;
 
-	while(nodePtr && count != pos){
+	while(nodePtr && count != pos){ // while valid or position is not reached then count
 		nodePtr = nodePtr->next;
 		count++;
 	}
 
+	// if position was reached then set the value at current node
 	if(count == pos){
 		nodePtr->value = var;
 	}
 
 }
 
+
+/* Description: Overloaded out stream operator
+ * Parameters: The stream and the output thing
+ */
 template<class T>
 ostream& operator<<(ostream& strm, Thing1<T> &output)
 {
+	// Set the pointer to the head 
 	ListNode<T>* nodePtr = output.head;
 
-	while(nodePtr){
+	while(nodePtr){ // while valid node keep traversing
 		strm<<nodePtr->value<<" ";
 		nodePtr = nodePtr->next;
 	}
--- Homework1/Program6/Thing2.h
+++ Homework1/Program6/Thing2.h
@@ -1,3 +1,8 @@
+/* Author: Cole Barbes
+ * Creation Date: 09/05/23
+ * Last Update: 09/07/23
+ * Description: This class implements an iterator over top of the Linked list done in Thing1
+ */
 #ifndef THING2_H
 #define THING2_H
 
@@ -7,49 +12,74 @@
 
 using namespace std;
 
+// implement inherited class Thing2 of Thing1
 template<class T>
 class Thing2: public Thing1<T>{
 	private:
-	ListNode<T>* iter;
+		// create pointer to iterate over the list
+		ListNode<T>* iter;
 	public:
+		// constructor and destructor
 		Thing2();
 		~Thing2();
+		// reset iterator back and front
 		void resetIteratorToFront();
 		void resetIteratorToBack();
+		// prefix and postfix operator overload incrementer
 		void operator++();
 		void operator++(int);
 		void operator--();
 		void operator--(int);
+		// overload the incremental += and -=
 		void operator+=(int);
 		void operator-=(int);
+		// get the value of the node pointed to by the iterator
 		T get();
+		// insert the value at the iterator 
 		void insertval(T);
+		// delete the value pointed to by the iterator
 		void deleteval();
 };
 
+/*
+ * Constructor
+ */
 template<class T> 
 Thing2<T>::Thing2()
 {
 	iter = nullptr;
 }
 
+/*
+ * Destructor
+ */
 template<class T>
 Thing2<T>::~Thing2()
 {
+	Thing1<T>::clear();
 }
 
+/*
+ * reset the iterator to the head
+ */
 template<class T>
 void Thing2<T>::resetIteratorToFront()
 {
 	iter = Thing1<T>::head;
 }
 
+/*
+ * reset the iterator to the tail
+ */
 template<class T>
 void Thing2<T>::resetIteratorToBack()
 {
 	iter = Thing1<T>::tail;
 }
 
+/*
+ * Increment the iterator one node down the list prefix
+ */
 template<class T>
 void Thing2<T>::operator++()
 {
@@ -59,6 +89,9 @@ void Thing2<T>::operator++()
 	
 }
 
+/*
+ * Increment the iterator one down the list postfix
+ */
 template<class T>
 void Thing2<T>::operator++(int)
 {
@@ -68,7 +101,9 @@ void Thing2<T>::operator++(int)
 	
 }
 
-
+/*
+ * decrement the iterator one up the list prefix
+ */
 template<class T>
 void Thing2<T>::operator--()
 {
@@ -77,6 +112,9 @@ void Thing2<T>::operator--()
 	}
 }
 
+/*
+ * Decrement the iterator one up the list postfix
+ */
 template<class T>
 void Thing2<T>::operator--(int)
 {
@@ -86,7 +124,10 @@ void Thing2<T>::operator--(int)
 	
 }
 
-
+/*
+ * Increment the Iterator n nodes down the list
+ * Parameters: the n number of nodes 
+ */
 template<class T>
 void Thing2<T>::operator+=(int n)
 {
@@ -94,11 +135,16 @@ void Thing2<T>::operator+=(int n)
 	if(iter){
 		while(count!=n&&iter->next){
 			iter = iter->next;
+			count++;
 		}
 	}
 		
 }
 
+/*
+ * Decrement the iterator n nodes up the list
+ * parameters: n number of nodes
+ */
 template<class T>
 void Thing2<T>::operator-=(int n)
 {
@@ -106,23 +152,35 @@ void Thing2<T>::operator-=(int n)
 	if(iter){
 		while(count!=n&&iter->previous){
 			iter = iter->previous;
+			count++;
 		}
 	}
 }
 
+/*
+ * Get the iterators value
+ * Return the value of the iterator
+ */
 template<class T>
 T Thing2<T>::get()
 {
 	return iter->value;
 }
 
+/*
+ * Insert the value given into the list at the location after the iterator and before the next node
+ * parameters: the templated value 
+ */
 template<class T>
 void Thing2<T>::insertval(T var)
 {
+	// create the new node
 	ListNode<T>* nodePtr = new ListNode<T>;
+	// create the temporary node to hold the iterators next
 	ListNode<T>* tempPtr;
 	nodePtr->value = var;
-
+	
+	// set all of the needed pointers to their respective nodes
 	tempPtr = iter->next;
 	iter->next = nodePtr;
 	nodePtr->previous = iter;
@@ -130,26 +188,38 @@ void Thing2<T>::insertval(T var)
 
 }
 
+/* 
+ * Delete the value pointed to by the iterator 
+ */
 template<class T> 
 void Thing2<T>::deleteval()
 {
-	if(!iter->next){
+	if(!iter->next){ // if theres no next node just delete
 		Thing1<T>::tail = Thing1<T>::tail->previous;
 		Thing1<T>::tail->next = nullptr;
 
 		delete iter;
 		iter = Thing1<T>::head;
 	}
-	else{
-		ListNode<T>* nodePtr;
-		ListNode<T>* previousNode;
-
-		previousNode = iter->previous;
-		nodePtr = iter->next;
-		previousNode->next = nodePtr;
-		nodePtr->previous = previousNode;
+	else if(iter==Thing1<T>::head){ // if the iterator is at the head then delete the head and move forward one
+		Thing1<T>::head = Thing1<T>::head->next;
 		delete iter;
 		iter = Thing1<T>::head;
+	}
+	else{ // all other cases move more memory addresses
+		//create the temporary pointers
+		ListNode<T>* nodePtr;
+		ListNode<T>* previousNode;
+		// save the previous and next node
+		previousNode = iter->previous;
+		nodePtr = iter->next;
+		
+		// set the last node next to the node before and the nexts previous to the next
+		previousNode->next = nodePtr;
+		nodePtr->previous = previousNode;
+		//delete the  node and set the iter to the next node
+		delete iter;
+		iter = nodePtr;
 	}
 }
 
